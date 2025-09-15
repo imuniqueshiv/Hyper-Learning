3d2f15fcdba7b22bbafab6f7634eebd68a20f842
Shiv <singhshivraj89969@gmail.com>
2025-09-15 23:20:42 +0530

    Add .gitignore to ignore backup files/folders

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Reusable function to inject HTML
-function loadHTML(id, file) {
-  fetch(file)
-    .then(response => {
-      if (!response.ok) throw new Error(`Failed to load ${file}`);
-      return response.text();
-    })
-    .then(data => {
-      document.getElementById(id).innerHTML = data;
-    })
-    .catch(err => console.error(err));
-}
-
-// Always load from root directory
-// loadHTML("header", "header.html");
-loadHTML("footer", "footer.html");
+document.getElementById("year").textContent = new Date().getFullYear();
